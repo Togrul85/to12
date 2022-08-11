82ba8cbcc7dd79c544caca6f456344a1113e5ea6
unknown <toghrul@code.edu.az>
2022-08-11 18:22:28 +0400

    .

---
+++ 1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccessModifiers.Home
+{
+    class Program
+    {
+        
+        static void Main(string [] args)
+        {
+            User user1 = new User("Toq");
+            user1.Password = "TOQRUL";
+        } 
+            
+        
+    }
+}
---
+++ 3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessModifiers.Home
+{
+    class User
+    {
+        public string Username { get; set; }
+        private string _password;
+        public string Password {
+            get {
+                return _password;
+            } 
+            set {
+                if (value.Length>=8 && value.Length <= 25)
+                {
+                   
+        public User(string userName)
+        {
+            if (userName.Length>=6 && userName.Length<25)
+            {
+                Username = userName;
+            }
+        }
+
+
+    }
+   
+}
+    
+
+
+    
+    
+    
+       
+    
+
+
+    
+   
+
+
